bb29898015e364bd5d8e04131475c2ab37047072
Keenan <keenanjj13@gmail.com>
2026-04-09 18:33:44 -0600

    Bump CacheFormatVersion to 4 for node-count validation metadata

--- src/Fingerprint/ModlistFingerprint.cs
+++ src/Fingerprint/ModlistFingerprint.cs
@@ -25,7 +25,7 @@ namespace FluxxField.DefLoadCache
         /// Bump this when the cache format changes. All caches with a different
         /// version are invalidated.
         /// </summary>
-        public const int CacheFormatVersion = 3;
+        public const int CacheFormatVersion = 4;
 
         internal static string Compute()
         {
