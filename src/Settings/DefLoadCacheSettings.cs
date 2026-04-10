c94ca2f71ea49ce8ebb6fe8b5ea8f98cdd561f5d
Keenan <keenanjj13@gmail.com>
2026-04-10 17:15:22 -0600

    Remove maxCachedProfiles setting, pruning is now profile-aware

--- src/Settings/DefLoadCacheSettings.cs
+++ src/Settings/DefLoadCacheSettings.cs
@@ -7,16 +7,18 @@ namespace FluxxField.DefLoadCache
         public bool cacheEnabled = true;
         public bool skipModFileLoading = true;
         public bool skipPatchApplication = true;
-        public int maxCachedProfiles = 10;
         public bool diagnosticDumpEnabled = false;
         public bool skipNextLaunch = false;
+
+        // Legacy field kept only for backwards compat when loading old settings files
+        private int _legacyMaxCachedProfiles = 10;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref cacheEnabled, "cacheEnabled", true);
             Scribe_Values.Look(ref skipModFileLoading, "skipModFileLoading", true);
             Scribe_Values.Look(ref skipPatchApplication, "skipPatchApplication", true);
-            Scribe_Values.Look(ref maxCachedProfiles, "maxCachedProfiles", 10);
+            Scribe_Values.Look(ref _legacyMaxCachedProfiles, "maxCachedProfiles", 10);
             Scribe_Values.Look(ref diagnosticDumpEnabled, "diagnosticDumpEnabled", false);
             Scribe_Values.Look(ref skipNextLaunch, "skipNextLaunch", false);
             base.ExposeData();
